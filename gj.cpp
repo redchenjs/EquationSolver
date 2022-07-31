7e0728292e6c4067cb3cac7d4ea3cf155b4bdd02
Jack Chen <redchenjs@live.com>
2022-07-31 22:06:08 +0800

    main: update gj dfa

--- gj.cpp
+++ gj.cpp
@@ -12,8 +12,6 @@
 
 using namespace std;
 
-const int N = 4;
-
 /*
  2  -3   1   5   6
 -3   1   2  -4   5
@@ -26,255 +24,174 @@ const int N = 4;
  x4 =  1
 */
 
-double C[N][N + 1] = {
-    [0] = { 2,  -3,   1,   5,   6},
-    [1] = {-3,   1,   2,  -4,   5},
-    [2] = {-1,  -2,   3,   1,  11},
-    [3] = { 0,   0,   1,   1,   2},
-};
-
-double D[N][N + 1] = { 0 };
-
-double E[7][7] = {
-    [0] = { 0,   0,   0,   0,   0,   0,   0},
-    [1] = { 2,  -3,   1,   5,   6,   0,   0},
-    [2] = {-3,   1,   2,  -4,   5,   0,   0},
-    [3] = {-1,  -2,   3,   1,  11,   0,   0},
-    [4] = { 0,   0,   1,   1,   2,   0,   0},
+double data[7][7] = {
+    [0] = { 2,  -3,   1,   5,   6,   0,   0},
+    [1] = {-3,   1,   2,  -4,   5,   0,   0},
+    [2] = {-1,  -2,   3,   1,  11,   0,   0},
+    [3] = { 0,   0,   1,   1,   2,   0,   0},
+    [4] = { 0,   0,   0,   0,   0,   0,   0},
     [5] = { 0,   0,   0,   0,   0,   0,   0},
     [6] = { 0,   0,   0,   0,   0,   0,   0},
 };
 
-double F[7] = { 0 };
-
-void solveEqual(double dEqualCoeff[7][7], int order, double *dAffinePara)
+void print_mat(double C[7][7])
 {
-    printf("========================================================= E initial\n");
+    printf("======================================================\n");
     for (int p = 0; p < 7; p++) {
         for (int q = 0; q < 7; q++) {
-            // dEqualCoeff[p][q] *= 256;
-            printf("%6.1f\t", dEqualCoeff[p][q]);
+            printf("%6.1f\t", C[p][q]);
         }
         printf("\n");
     }
+}
 
-    for (int k = 0; k < order; k++) {
-        dAffinePara[k] = 0.;
-    }
-
-    // row echelon
-    for (int i = 1; i < order; i++) {
-        // find column max
-        double temp = fabs(dEqualCoeff[i][i - 1]);
-        int tempIdx = i;
-        for (int j = i + 1; j < order + 1; j++) {
-            if ( fabs(dEqualCoeff[j][i - 1]) > temp ) {
-                temp = fabs(dEqualCoeff[j][i - 1]);
-                tempIdx = j;
-            }
-        }
-
-        // swap line
-        if ( tempIdx != i ) {
-            for (int j = 0; j < order + 1; j++) {
-                dEqualCoeff[0][j] = dEqualCoeff[i][j];
-                dEqualCoeff[i][j] = dEqualCoeff[tempIdx][j];
-                dEqualCoeff[tempIdx][j] = dEqualCoeff[0][j];
-            }
-        }
-
-        // elimination first column
-        if ( dEqualCoeff[i][i - 1] == 0. ) {
-            printf("========================================================= !!!\n");
-            printf("return at (dEqualCoeff[%d][%d - 1] == 0.)\n", i, i);
-            return;
-        }
-        for (int j = i + 1; j < order + 1; j++) {
-            for (int k = i; k < order + 1; k++) {
-                dEqualCoeff[j][k] = dEqualCoeff[j][k] - dEqualCoeff[i][k] * dEqualCoeff[j][i - 1] / dEqualCoeff[i][i - 1];
-            }
-        }
-
-        printf("========================================================= E i%d\n", i);
-        for (int p = 0; p < 7; p++) {
-            for (int q = 0; q < 7; q++) {
-                printf("%6.1f\t", dEqualCoeff[p][q]);
-            }
-            printf("\n");
-        }
-    }
-
-    if (dEqualCoeff[order][order - 1] == 0.) {
-        printf("========================================================= ???\n");
-        printf("return at (dEqualCoeff[%d][%d - 1] == 0.)\n", order, order);
-        return;
-    }
-    printf("========================================================= E final\n");
+void print_mat(int i, int j, int k, double C[7][7])
+{
+    printf("====================================================== i = %d j = %d k = %d\n", i, j, k);
     for (int p = 0; p < 7; p++) {
         for (int q = 0; q < 7; q++) {
-            printf("%6.1f\t", dEqualCoeff[p][q]);
-        }
-        printf("\n");
-    }
-    dAffinePara[order - 1] = dEqualCoeff[order][order] / dEqualCoeff[order][order - 1];
-    for (int i = order - 2; i >= 0; i--) {
-        if ( dEqualCoeff[i + 1][i] == 0. ) {
-            for (int k = 0; k < order; k++) {
-                dAffinePara[k] = 0.;
-            }
-            printf("========================================================= >>>\n");
-            printf("return at (dEqualCoeff[%d + 1][%d] == 0.)\n", i, i);
-            return;
-        }
-        double temp = 0;
-        for (int j = i + 1; j < order; j++) {
-            temp += dEqualCoeff[i + 1][j] * dAffinePara[j];
-        }
-        dAffinePara[i] = (dEqualCoeff[i + 1][order] - temp) / dEqualCoeff[i + 1][i];
-    }
-
-    printf("========================================================= results\n");
-    for (int i = 0; i < order; i++) {
-        printf("x%d = %6.1f\n", i + 1, dAffinePara[i]);
-    }
-}
-
-void method_vtm(int n)
-{
-    solveEqual(E, n, F);
-}
-
-void method_1sdf(int n, double D[N][N + 1], double C[N][N + 1])
-{
-    printf("========================================================= C initial\n");
-    for (int p = 0; p < n; p++) {
-        for (int q = 0; q < n + 1; q++) {
-            // C[p][q] *= 256;
             printf("%6.1f\t", C[p][q]);
         }
         printf("\n");
     }
+}
 
-    printf("========================================================= D initial\n");
-    for (int p = 0; p < n; p++) {
-        for (int q = 0; q < n + 1; q++) {
-            // D[p][q] *= 256;
-            printf("%6.1f\t", D[p][q]);
-        }
-        printf("\n");
+void print_res(bool param6, double C[7][7])
+{
+    int n = param6 ? 6 : 4;
+
+    printf("======================================================\n");
+    for (int i = 0; i < n; i++) {
+        printf("x%d = %6.1f\n", i, C[i][n] / C[i][i]);
     }
+}
+
+void method_gja(bool param6, bool pivoting, double C[7][7])
+{
+    int n = param6 ? 6 : 4;
+    double D[7][7] = { 0 };
 
     for (int k = 0; k < n; k++) {
-        for (int i = 0; i < n; i++) {           // Row
-            for (int j = 0; j < n + 1; j++) {   // Column
-                if (k == i) {
-                    D[i][j] = C[k][j];
-                } else {
-                    D[i][j] = (C[k][k] * C[i][j]) - (C[i][k] * C[k][j]);
+        if (pivoting) {
+            // find column max
+            int m = k;
+            double t = fabs(C[k][k]);
+
+            for (int i = k + 1; i < n; i++) {
+                if (fabs(C[i][k]) > t) {
+                    t = fabs(C[i][k]);
+                    m = i;
                 }
             }
-
-            printf("========================================================= D k%d i%d\n", k, i);
-            for (int p = 0; p < N; p++) {
-                for (int q = 0; q < N + 1; q++) {
-                    printf("%6.1f\t", D[p][q]);
+            print_mat(m, 0, k, C);
+            // swap rows k and m
+            if (m != k) {
+                for (int j = 0; j < n + 1; j++) {
+                    C[6][j] = C[k][j];
+                    C[k][j] = C[m][j];
+                    C[m][j] = C[6][j];
                 }
-                printf("\n");
+                print_mat(m, 1, k, C);
             }
         }
 
-        for (int p = 0; p < n; p++) {           // matrix C
-            for (int q = 0; q < n + 1; q++) {
-                C[p][q] = D[p][q];
+        double M = C[k][k];
+        for (int i = 0; i < n; i++) {
+            double L = C[i][k];
+
+            if (k == i) {
+                // make C[i][k] one
+                for (int j = 0; j < n + 1; j++) {
+                    D[k][j] = C[k][j] / M;
+                }
+            } else {
+                // make C[i][k] zero
+                for (int j = 0; j < n + 1; j++) {
+                    D[i][j] = C[i][j] - (L / M) * C[k][j];
+                }
+            }
+        }
+
+        printf("D:");
+        print_mat(D);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                C[i][j] = D[i][j];
             }
         }
     }
 
-    printf("========================================================= C final\n");
-    for (int p = 0; p < n; p++) {
-        for (int q = 0; q < n + 1; q++) {
-            printf("%6.1f\t", C[p][q]);
-        }
-        printf("\n");
-    }
-
-    printf("========================================================= D final\n");
-    for (int p = 0; p < n; p++) {
-        for (int q = 0; q < n + 1; q++) {
-            printf("%6.1f\t", D[p][q]);
-        }
-        printf("\n");
-    }
+    printf("C:");
+    print_mat(C);
+    print_res(param6, C);
 }
 
-void method_2sdf(int n, double D[N][N + 1], double C[N][N + 1])
+void method_dfa(bool param6, bool pivoting, double C[7][7])
 {
-    printf("========================================================= C initial\n");
-    for (int p = 0; p < n; p++) {
-        for (int q = 0; q < n + 1; q++) {
-            // C[p][q] *= 256;
-            printf("%6.1f\t", C[p][q]);
-        }
-        printf("\n");
-    }
-
-    printf("========================================================= D initial\n");
-    for (int p = 0; p < n; p++) {
-        for (int q = 0; q < n + 1; q++) {
-            // D[p][q] *= 256;
-            printf("%6.1f\t", D[p][q]);
-        }
-        printf("\n");
-    }
+    int n = param6 ? 6 : 4;
+    double D[7][7] = { 0 };
 
     for (int k = 0; k < n; k++) {
-        for (int i = 0; i < n; i++) {           // Row
-            for (int j = 0; j < n + 1; j++) {   // Column
-                if (k == i) {
-                    D[i][j] = C[k][j];
-                } else {
-                    D[i][j] = (C[k][k] * C[i][j]) - (C[i][k] * C[k][j]);
+        if (pivoting) {
+            // find column max
+            int m = k;
+            double t = fabs(C[k][k]);
+
+            for (int i = k + 1; i < n; i++) {
+                if (fabs(C[i][k]) > t) {
+                    t = fabs(C[i][k]);
+                    m = i;
                 }
             }
-
-            printf("========================================================= D k%d i%d\n", k, i);
-            for (int p = 0; p < N; p++) {
-                for (int q = 0; q < N + 1; q++) {
-                    printf("%6.1f\t", D[p][q]);
+            print_mat(m, 0, k, C);
+            // swap rows k and m
+            if (m != k) {
+                for (int j = 0; j < n + 1; j++) {
+                    C[6][j] = C[k][j];
+                    C[k][j] = C[m][j];
+                    C[m][j] = C[6][j];
                 }
-                printf("\n");
+                print_mat(m, 1, k, C);
             }
         }
 
-        for (int p = 0; p < n; p++) {           // matrix C
-            for (int q = 0; q < n + 1; q++) {
-                C[p][q] = D[p][q];
+        double M = C[k][k];
+        for (int i = 0; i < n; i++) {
+            double L = C[i][k];
+
+            if (k == i) {
+                // row k is not modified
+                for (int j = 0; j < n + 1; j++) {
+                    D[k][j] = C[k][j];
+                }
+            } else {
+                // make C[i][k] zero
+                for (int j = 0; j < n + 1; j++) {
+                    D[i][j] = M * C[i][j] - L * C[k][j];
+                }
+            }
+        }
+
+        printf("D:");
+        print_mat(D);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                C[i][j] = D[i][j];
             }
         }
     }
 
-    printf("========================================================= C final\n");
-    for (int p = 0; p < n; p++) {
-        for (int q = 0; q < n + 1; q++) {
-            printf("%6.1f\t", C[p][q]);
-        }
-        printf("\n");
-    }
-
-    printf("========================================================= D final\n");
-    for (int p = 0; p < n; p++) {
-        for (int q = 0; q < n + 1; q++) {
-            printf("%6.1f\t", D[p][q]);
-        }
-        printf("\n");
-    }
+    printf("C:");
+    print_mat(C);
+    print_res(param6, C);
 }
 
 int main(int argc, char **argv)
 {
-    // method_vtm(N);
-    // method_1sdf(N, D, C);
-    method_2sdf(N, D, C);
+    // method_gja(false, true, data);
+    method_dfa(false, true, data);
 
     return 0;
 }
