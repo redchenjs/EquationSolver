87ff756dba72c7d580d676799deaa3a6989709d0
Jack Chen <redchenjs@live.com>
2022-08-07 22:12:41 +0800

    main: update gj dfa2

--- gj.cpp
+++ gj.cpp
@@ -122,6 +122,17 @@ void print_res(bool param6, double C[7][7])
     }
 }
 
+void print_res(int a, bool param6, double C[7][7])
+{
+    int n = param6 ? 6 : 4;
+
+    printf("--------------------------------RESULT-----------------------------------\n");
+    for (int i = 0; i < n; i++) {
+        printf(LOG_COLOR_I "x%d = %8.1f", i, C[i][n] / C[i][i]);
+        printf(LOG_RESET_COLOR "\n");
+    }
+}
+
 void method_gja(bool param6, bool pivoting, double C[7][7])
 {
     int n = param6 ? 6 : 4;
@@ -188,7 +199,7 @@ void method_gja(bool param6, bool pivoting, double C[7][7])
 void method_dfa(bool param6, bool pivoting, double C[7][7])
 {
     int n = param6 ? 6 : 4;
-    double D[7][7] = { 0 };
+    int D[7][7] = { 0 };
 
     print_mat('I', param6, C);
 
@@ -196,7 +207,7 @@ void method_dfa(bool param6, bool pivoting, double C[7][7])
         if (pivoting) {
             // find column max
             int m = k;
-            double t = fabs(C[k][k]);
+            int t = fabs(C[k][k]);
 
             for (int i = k + 1; i < n; i++) {
                 if (fabs(C[i][k]) > t) {
@@ -219,9 +230,9 @@ void method_dfa(bool param6, bool pivoting, double C[7][7])
             }
         }
 
-        double M = C[k][k];
+        int M = C[k][k];
         for (int i = 0; i < n; i++) {
-            double L = C[i][k];
+            int L = C[i][k];
 
             if (k == i) {
                 // row k is not modified
@@ -248,10 +259,84 @@ void method_dfa(bool param6, bool pivoting, double C[7][7])
     print_res(param6, C);
 }
 
+void method_dfa2(bool param6, bool pivoting, double C[7][7])
+{
+    int n = param6 ? 6 : 4;
+    int D[7][7] = { 0 };
+
+    print_mat('I', param6, C);
+
+    for (int k = 0; k < n; k++) {
+        if (pivoting) {
+            // find column max
+            int m = k;
+            int t = fabs(C[k][k]);
+
+            for (int i = k + 1; i < n; i++) {
+                if (fabs(C[i][k]) > t) {
+                    t = fabs(C[i][k]);
+                    m = i;
+                }
+            }
+
+            // swap rows k and m
+            if (m != k) {
+                print_mat('A', k, m, param6, C);
+
+                for (int j = 0; j < n + 1; j++) {
+                    C[6][j] = C[k][j];
+                    C[k][j] = C[m][j];
+                    C[m][j] = C[6][j];
+                }
+
+                print_mat('B', k, m, param6, C);
+            }
+        }
+
+        int M = C[k][k];
+        int B = 0;
+        int TM = abs(M);
+
+        printf("TM: %d, B: %d\n", TM, B);
+        while (TM >>= 1) {
+            B++;
+            printf("TM: %d, B: %d\n", TM, B);
+        }
+
+        for (int i = 0; i < n; i++) {
+            int L = C[i][k];
+
+            if (k == i) {
+                // row k is not modified
+                for (int j = 0; j < n + 1; j++) {
+                    D[k][j] = C[k][j];
+                }
+            } else {
+                // make C[i][k] zero
+                for (int j = 0; j < n + 1; j++) {
+                    D[i][j] = M * C[i][j] - L * C[k][j];
+                    D[i][j] = D[i][j] >> B;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n + 2; j++) {
+                C[i][j] = D[i][j];
+            }
+        }
+
+        print_mat('C', k, param6, C);
+    }
+
+    print_res(0, param6, C);
+}
+
 int main(int argc, char **argv)
 {
     // method_gja(false, true, data);
-    method_dfa(false, true, data);
+    // method_dfa(false, true, data);
+    method_dfa2(false, true, data);
 
     return 0;
 }
