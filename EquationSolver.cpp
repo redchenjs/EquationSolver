12bbcc3d95a8805a4f6b3aa41e53dc268a513035
Jack Chen <redchenjs@live.com>
2022-09-22 21:09:27 +0800

    main: rename to EquationSolver

--- EquationSolver.cpp
+++ EquationSolver.cpp
@@ -1,14 +1,11 @@
 /*
- * gj.cpp
+ * EquationSolver.cpp
  *
  *  Created on: 2022-07-30 18:20
  *      Author: Jack Chen <redchenjs@live.com>
  */
 
-#include <cmath>
-#include <cstdio>
-#include <cstring>
-#include <algorithm>
+#include "EquationSolver.h"
 
 using namespace std;
 
@@ -26,42 +23,9 @@ using namespace std;
 #define LOG_COLOR_W       LOG_COLOR(LOG_COLOR_BROWN)
 #define LOG_COLOR_I       LOG_COLOR(LOG_COLOR_GREEN)
 
-/*
- 2  -3   1   5   6
--3   1   2  -4   5
--1  -2   3   1  11
- 0   0   1   1   2
-
- x0 = -3
- x1 = -2
- x2 =  1
- x3 =  1
-*/
-
-/*
- 0   1   2   2
- 1   1   1   1
- 0  -1  -4  -2
- 3   2   1  -1
-
- x0 = -2
- x1 = -3
- x2 = -1
- x3 =  0
-*/
-
-const double data[7][7] = {
-    [0] = { 0,   1,   2,   2,   1,   0,   0},
-    [1] = { 1,   1,   1,   1,   0,   0,   0},
-    [2] = { 0,  -1,  -4,  -2,   1,   0,   0},
-    [3] = { 3,   2,   1,  -1,  -1,   0,   0},
-    [4] = { 0,   0,   0,   0,   0,   0,   0},
-    [5] = { 0,   0,   0,   0,   0,   0,   0},
-    [6] = { 0,   0,   0,   0,   0,   0,   0},
-};
-
-void print_mat(const char *str, bool param6, double C[7][7])
+void EquationSolver::print_mat(const char *str, bool param6, double C[7][7])
 {
+#ifdef DEBUG
     int n = param6 ? 6 : 4;
 
     printf("------------------------------- %-4s ------------------------------ n = %d\n", str, n);
@@ -75,10 +39,12 @@ void print_mat(const char *str, bool param6, double C[7][7])
         printf(LOG_COLOR_W "%8.1f", C[p][n]);
         printf(LOG_RESET_COLOR "\n");
     }
+#endif
 }
 
-void print_mat(char idx, int k, bool param6, double C[7][7])
+void EquationSolver::print_mat(char idx, int k, bool param6, double C[7][7])
 {
+#ifdef DEBUG
     int n = param6 ? 6 : 4;
 
     printf("------------------------------- MATRIX %c -------------------- n = %d k = %d\n", idx, n, k);
@@ -92,10 +58,12 @@ void print_mat(char idx, int k, bool param6, double C[7][7])
         printf(LOG_COLOR_W "%8.1f", C[p][n]);
         printf(LOG_RESET_COLOR "\n");
     }
+#endif
 }
 
-void print_mat(char idx, int k, int m, bool param6, double C[7][7])
+void EquationSolver::print_mat(char idx, int k, int m, bool param6, double C[7][7])
 {
+#ifdef DEBUG
     int n = param6 ? 6 : 4;
 
     printf("------------------------------- MATRIX %c -------------- n = %d k = %d m = %d\n", idx, n, k, m);
@@ -109,10 +77,12 @@ void print_mat(char idx, int k, int m, bool param6, double C[7][7])
         printf(LOG_COLOR_W "%8.1f", C[p][n]);
         printf(LOG_RESET_COLOR "\n");
     }
+#endif
 }
 
-void print_res(bool param6, double C[7][7])
+void EquationSolver::print_res(bool param6, double C[7][7])
 {
+#ifdef DEBUG
     int n = param6 ? 6 : 4;
 
     printf("------------------------------- RESULT ----------------------------------\n");
@@ -120,51 +90,26 @@ void print_res(bool param6, double C[7][7])
         printf(LOG_COLOR_I "x%d = %8.1f", i, C[i][n] / C[i][i]);
         printf(LOG_RESET_COLOR "\n");
     }
+#endif
 }
 
-void print_res(int a, bool param6, double C[7][7])
+void EquationSolver::print_res(bool param6, double C[7][7], int scale)
 {
+#ifdef DEBUG
     int n = param6 ? 6 : 4;
 
     printf("------------------------------- RESULT ----------------------------------\n");
     for (int i = 0; i < n; i++) {
-        printf(LOG_COLOR_I "x%d = %8.1f", i, C[i][n] / C[i][i]);
+        printf(LOG_COLOR_I "x%d = %8.1f", i, C[i][n] / C[i][i] / pow(2.0, scale));
         printf(LOG_RESET_COLOR "\n");
     }
+#endif
 }
 
-void scale_mat(int b, bool param6, double C[7][7])
+void EquationSolver::method_gja(bool param6, bool pivoting)
 {
     int n = param6 ? 6 : 4;
-
-    for (int p = 0; p < n; p++) {
-        for (int q = 0; q < n; q++) {
-            if (b >= 0) {
-                C[p][q] = C[p][q] * pow(2, b);
-            } else {
-                C[p][q] = C[p][q] / pow(2, b);
-            }
-        }
-    }
-}
-
-void scale_mat(int a, int b, bool param6, double C[7][7])
-{
-    int n = param6 ? 6 : 4;
-
-    for (int p = 0; p < n; p++) {
-        for (int q = 0; q < n - 1; q++) {
-
-            C[p][q] = C[p][q] * pow(2, a);
-        }
-        C[p][n] = C[p][n] * pow(2, b);
-    }
-}
-
-void method_gja(bool param6, bool pivoting, double C[7][7])
-{
-    int n = param6 ? 6 : 4;
-    double D[7][7] = { 0 };
+    double D[7][7] = { 0.0 };
 
     print_mat("GJA", param6, C);
 
@@ -196,6 +141,7 @@ void method_gja(bool param6, bool pivoting, double C[7][7])
         }
 
         double M = C[k][k];
+
         for (int i = 0; i < n; i++) {
             double L = C[i][k];
 
@@ -224,7 +170,7 @@ void method_gja(bool param6, bool pivoting, double C[7][7])
     print_res(param6, C);
 }
 
-void method_dfa(bool param6, bool pivoting, double C[7][7])
+void EquationSolver::method_dfa(bool param6, bool pivoting, int scale)
 {
     int n = param6 ? 6 : 4;
     int64_t D[7][7] = { 0 };
@@ -259,6 +205,7 @@ void method_dfa(bool param6, bool pivoting, double C[7][7])
         }
 
         int64_t M = C[k][k];
+
         for (int i = 0; i < n; i++) {
             int64_t L = C[i][k];
 
@@ -284,15 +231,14 @@ void method_dfa(bool param6, bool pivoting, double C[7][7])
         print_mat('C', k, param6, C);
     }
 
-    print_res(param6, C);
+    print_res(param6, C, scale);
 }
 
-void method_dfa2(bool param6, bool pivoting, double C[7][7])
+void EquationSolver::method_dfa2(bool param6, bool pivoting, int scale)
 {
     int n = param6 ? 6 : 4;
     int64_t D[7][7] = { 0 };
 
-    scale_mat(0, 3, param6, C);
     print_mat("DFA2", param6, C);
 
     for (int k = 0; k < n; k++) {
@@ -356,21 +302,105 @@ void method_dfa2(bool param6, bool pivoting, double C[7][7])
         print_mat('C', k, param6, C);
     }
 
-    print_res(0, param6, C);
+    print_res(param6, C, scale);
 }
 
-int main(int argc, char **argv)
+void EquationSolver::method_dfa3(bool param6, bool pivoting, int scale)
 {
-    double C[7][7] = {0};
+    int n = param6 ? 6 : 4;
+    int64_t D[7][7] = { 0 };
 
-    memcpy(C, data, sizeof(C));
-    method_gja(false, true, C);
+    print_mat("DFA3", param6, C);
 
-    memcpy(C, data, sizeof(C));
-    method_dfa(false, true, C);
+    for (int k = 0; k < n; k++) {
+        if (pivoting) {
+            // find column max
+            int m = k;
+            int64_t t = fabs(C[k][k]);
 
-    memcpy(C, data, sizeof(C));
-    method_dfa2(false, true, C);
+            for (int i = k + 1; i < n; i++) {
+                if (fabs(C[i][k]) > t) {
+                    t = fabs(C[i][k]);
+                    m = i;
+                }
+            }
 
-    return 0;
+            // swap rows k and m
+            if (m != k) {
+                print_mat('A', k, m, param6, C);
+
+                for (int j = 0; j < n + 1; j++) {
+                    C[6][j] = C[k][j];
+                    C[k][j] = C[m][j];
+                    C[m][j] = C[6][j];
+                }
+
+                print_mat('B', k, m, param6, C);
+            }
+        }
+
+        int B = 0;
+        int64_t M = C[k][k];
+        int64_t TM = abs(M);
+        int64_t HM = abs(M);
+
+        while (TM >>= 1) {
+            B++;
+        }
+
+        if (B != 0) {
+            HM >>= B - 1;
+
+            if (HM & 0x01) {
+                B++;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            int64_t L = C[i][k];
+
+            if (k == i) {
+                // row k is not modified
+                for (int j = 0; j < n + 1; j++) {
+                    D[k][j] = C[k][j];
+                }
+            } else {
+                // make C[i][k] zero
+                for (int j = 0; j < n + 1; j++) {
+                    D[i][j] = M * C[i][j] - L * C[k][j];
+                    D[i][j] = D[i][j] >> B;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n + 2; j++) {
+                C[i][j] = D[i][j];
+            }
+        }
+
+        print_mat('C', k, param6, C);
+    }
+
+    print_res(param6, C, scale);
+}
+
+void EquationSolver::load_data(const int64_t i64EqualCoeff[7][7], int iParaNum, int scale)
+{
+    for (int row = 0; row < iParaNum; row++) {
+        for (int i = 0; i < iParaNum + 1; i++) {
+            if (i == iParaNum) {
+                C[row][i] = i64EqualCoeff[row + 1][i] << scale;
+            } else {
+                C[row][i] = i64EqualCoeff[row + 1][i];
+            }
+        }
+    }
+}
+
+void EquationSolver::save_data(double dAffinePara[6], int iParaNum, int scale)
+{
+    for (int i = 0; i < iParaNum; i++) {
+        dAffinePara[i] = C[i][iParaNum] / C[i][i] / pow(2.0, scale);
+    }
 }
---
+++ main.cpp
@@ -0,0 +1,69 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 2022-07-30 18:20
+ *      Author: Jack Chen <redchenjs@live.com>
+ */
+
+#include "EquationSolver.h"
+
+/*
+ 2  -3   1   5   6
+-3   1   2  -4   5
+-1  -2   3   1  11
+ 0   0   1   1   2
+
+ x0 = -3
+ x1 = -2
+ x2 =  1
+ x3 =  1
+*/
+
+/*
+ 0   1   2   2   1
+ 1   1   1   1   0
+ 0  -1  -4  -2   1
+ 3   2   1  -1  -1
+
+ x0 = -2
+ x1 = -3
+ x2 = -1
+ x3 =  0
+*/
+
+static const int64_t C[7][7] = {
+    [0] = { 0,   0,   0,   0,   0,   0,   0},
+    [1] = { 0,   1,   2,   2,   1,   0,   0},
+    [2] = { 1,   1,   1,   1,   0,   0,   0},
+    [3] = { 0,  -1,  -4,  -2,   1,   0,   0},
+    [4] = { 3,   2,   1,  -1,  -1,   0,   0},
+    [5] = { 0,   0,   0,   0,   0,   0,   0},
+    [6] = { 0,   0,   0,   0,   0,   0,   0},
+};
+
+static double D[6] = { 0.0 };
+
+int main(int argc, char **argv)
+{
+    EquationSolver *solver = new EquationSolver();
+
+    solver->load_data(C, 4, 0);
+    solver->method_gja(false, true);
+    solver->save_data(D, 4, 0);
+
+    solver->load_data(C, 4, 0);
+    solver->method_dfa(false, true, 0);
+    solver->save_data(D, 4, 0);
+
+    solver->load_data(C, 4, 3);
+    solver->method_dfa2(false, true, 3);
+    solver->save_data(D, 4, 3);
+
+    solver->load_data(C, 4, 3);
+    solver->method_dfa3(false, true, 3);
+    solver->save_data(D, 4, 3);
+
+    delete(solver);
+
+    return 0;
+}
